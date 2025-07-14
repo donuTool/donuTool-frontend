3dba1af661608f8188e1691523303ca309320f55
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-14 20:04:49 +0900

    chore: 테마 토글 버튼 위치 조정 #1

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -12,7 +12,7 @@ export default function ThemeToggleButton() {
   return (
     <button
       onClick={toggleTheme}
-      className="dark:bg-donutool-button dark:text-donutool-text absolute right-3 bottom-3 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-2 py-2 text-xs font-semibold text-neutral-600 shadow transition duration-300 hover:shadow-md"
+      className="dark:bg-donutool-button dark:text-donutool-text absolute -right-14 bottom-4 flex cursor-pointer items-center justify-center rounded-full bg-gray-100 p-1 px-2 py-2 text-xs font-semibold text-neutral-600 shadow-md transition duration-300"
     >
       {isDarkMode ? (
         <SunIcon className="stroke-donutool-text h-6 w-6" />
