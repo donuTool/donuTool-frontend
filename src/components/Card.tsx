cb8e8138194372df5add20f4333c89e72d15b97d
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-17 15:11:40 +0900

    chore: 스타일 세부 조정 #1

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,7 +13,7 @@ export default function Card({
     <div
       className={`dark:bg-donutool-middle relative flex w-[${width ? width : 100}%] h-[${height ? height : 100}%] items-center justify-center rounded-2xl bg-gray-200 px-5 py-7 transition duration-300`}
     >
-      <div className="dark:text-donutool-bright absolute top-3.5 left-5 text-lg font-semibold text-neutral-600 transition duration-300">
+      <div className="dark:text-donutool-bright absolute top-3.5 left-5.5 text-lg font-semibold text-neutral-600 transition duration-300">
         {title}
       </div>
       {children}
