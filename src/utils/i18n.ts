5b34a0554e4d3fa34b25be1cd4b3cf74b1ee4fc4
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-07-28 19:00:44 +0900

    refactor: 언어별 텍스트 데이터 JSON 파일로 분리 #2

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,65 +1,15 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import english from "@/locales/englishText.json";
+import korean from "@/locales/koreanText.json";
 
 const resources = {
   en: {
-    translation: {
-      dashboard: "Dashboard",
-      help: "Help",
-      account: "Account",
-      stats: "Analytics",
-      settings: "Settings",
-      user: "User",
-      loggedInTime: "Logged in at",
-      usedTime: "Time used",
-      welcome: "Welcome, {{name}}!",
-      buttonsSetting: "Buttons Setting",
-      faq: "FAQ",
-      quickStartGuide: "Quick Start Guide",
-      contactUs: "Problem?",
-      contact: "Contact",
-      version: "Version",
-      community: "Community",
-      question: "Question {{number}}",
-      topThreeButtons: "Top 3 Most Used Buttons",
-      recentUsedButtons: "Button Clicks in the Last 7 Days",
-      usedTimePerDay: "Daily Usage Time",
-      frequentlyVisitiedWebsites: "Top 3 Frequently Visited Websites",
-      eraseData: "Clear Data",
-      toolBar: "Toggle Toolbar",
-      darkTheme: "Toggle Theme",
-      buttonsList: "Buttons List",
-    },
+    translation: english,
   },
   ko: {
-    translation: {
-      dashboard: "대시보드",
-      help: "도움말",
-      account: "계정",
-      stats: "통계",
-      settings: "설정",
-      user: "사용자",
-      loggedInTime: "로그인한 시간",
-      usedTime: "사용 시간",
-      welcome: "{{name}}님, 환영합니다!",
-      buttonsSetting: "버튼 세팅",
-      faq: "자주 묻는 질문",
-      quickStartGuide: "빠른 시작 가이드",
-      contactUs: "문의",
-      contact: "문의하기",
-      version: "버전",
-      community: "커뮤니티",
-      question: "질문 {{number}}",
-      topThreeButtons: "가장 많이 쓴 버튼 TOP 3",
-      recentUsedButtons: "최근 7일 간 버튼 클릭 횟수",
-      usedTimePerDay: "하루 사용 시간",
-      frequentlyVisitiedWebsites: "자주 방문한 웹사이트 TOP 3",
-      eraseData: "데이터 초기화",
-      toolBar: "툴바",
-      darkTheme: "테마",
-      buttonsList: "버튼 목록",
-    },
+    translation: korean,
   },
 };
 
