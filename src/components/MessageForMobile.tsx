8f772945446ba6e8b2325b398c9107bb3c8d7f6b
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-09-05 16:11:47 +0900

    chore: 스타일 수정 #1

--- src/components/MessageForMobile.tsx
+++ src/components/MessageForMobile.tsx
@@ -17,9 +17,9 @@ export default function MessageForMobile() {
   return (
     <>
       {isDeviceMobile ? (
-        <div className="absolute z-100 flex h-full w-full items-center justify-center bg-white/50 text-center text-[1vw] whitespace-pre-line text-neutral-600 backdrop-blur-lg">
+        <div className="absolute z-100 flex h-full w-full items-center justify-center bg-white/50 text-center text-[4vw] whitespace-pre-line text-neutral-600 backdrop-blur-lg">
           {
-            "현재 접속하신 모바일 환경에서는 지원되지 않는 사이트입니다!\n데스크톱을 이용해주세요."
+            "현재 접속하신 모바일 환경에서는\n지원되지 않는 사이트입니다!\n데스크톱을 이용해주세요."
           }
         </div>
       ) : (
