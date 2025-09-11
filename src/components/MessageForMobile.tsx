dfc6c757d47bfd206be541f93054ade816b5f876
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-09-11 15:16:18 +0900

    feat: 화면 너비 1200px 미만 시 UI 비활성화 및 안내 메시지 표시 #3

--- src/components/MessageForMobile.tsx
+++ src/components/MessageForMobile.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next";
 export default function MessageForMobile() {
   const { t } = useTranslation();
   const [isDeviceMobile, setIsDeviceMobile] = useState(false);
+  const [isWidthShort, setIsWidthShort] = useState(false);
 
   useEffect(() => {
     const checkIfMobile = () => {
@@ -16,11 +17,27 @@ export default function MessageForMobile() {
     checkIfMobile();
   }, []);
 
+  useEffect(() => {
+    const checkIfWidthShort = () => {
+      setIsWidthShort(window.innerWidth <= 1200);
+    };
+    checkIfWidthShort();
+    window.addEventListener("resize", checkIfWidthShort);
+
+    return () => {
+      window.removeEventListener("resize", checkIfWidthShort);
+    };
+  }, []);
+
   return (
     <>
       {isDeviceMobile ? (
         <div className="absolute z-100 flex h-full w-full items-center justify-center bg-white/50 text-center text-[4vw] whitespace-pre-line text-neutral-600 backdrop-blur-lg">
           {t("alertForMobile")}
+        </div>
+      ) : isWidthShort ? (
+        <div className="absolute z-100 flex h-full w-full items-center justify-center bg-white/50 text-center text-[2vw] whitespace-pre-line text-neutral-600 backdrop-blur-lg">
+          {t("alertForWidthShort")}
         </div>
       ) : (
         ""
