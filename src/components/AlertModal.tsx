b7d600b384ea16e3c052e4f3aca4003b48b1bc0c
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-09-23 18:42:03 +0900

    fix: 게스트 모드일 시 나타나는 확장프로그램 설치 안내 모달 표시 방법 변경 #4
    - 변경 방식 : 화면 정중앙 모달 표시 -> 화면 우하단의 토글 가능한 메세지
    - 사용자 사용 흐름 측면에서 개선

--- src/components/AlertModal.tsx
+++ src/components/AlertModal.tsx
@@ -2,48 +2,77 @@ import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { useUserStore } from "@/stores/useUserStore";
 import ReusableButton from "@/components/buttons/ReusableButton";
+import CloseIcon from "@/assets/close.svg";
+import ArrowLeftIcon from "@/assets/arrow-left.svg";
 
 export default function AlertModal() {
   const { t } = useTranslation();
   const googleId = useUserStore((state) => state.googleId);
   const [shouldRender, setShouldRender] = useState(false);
   const [isVisible, setIsVisible] = useState(!googleId);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   useEffect(() => {
     if (googleId === undefined) return;
 
     if (!googleId) {
       setIsVisible(true);
+      setIsCollapsed(false);
     } else {
       setIsVisible(false);
       setShouldRender(true);
     }
   }, [googleId]);
 
+  const handleClose = () => {
+    setIsCollapsed(true);
+  };
+
+  const handleShow = () => {
+    setIsCollapsed(false);
+  };
+
   if (shouldRender) return null;
 
   return (
-    <div
-      className={`absolute z-50 flex h-full w-full cursor-pointer items-center justify-center bg-black/50 backdrop-blur-xs transition-opacity duration-300 ${isVisible ? "opacity-100" : "opacity-0"} `}
-      onClick={() => {
-        setIsVisible(false);
-        setTimeout(() => setShouldRender(true), 300);
-      }}
-    >
-      <div
-        className={`dark:bg-donutool-middle dark:text-donutool-bright flex transform flex-col items-center justify-center gap-[1vw] rounded-[1vw] bg-gray-200 px-[4vw] py-[2vw] text-center text-[1vw] font-medium whitespace-pre-line text-neutral-600 transition-transform duration-300 ${isVisible ? "scale-100" : "scale-95"} `}
-      >
-        {t("textInModal")}
-        <ReusableButton
-          name={t("goToExtensionStore")}
-          onClick={() => {
-            window.open(
-              "https://chromewebstore.google.com/detail/donutool/heabnlejdfhggjiapbfmbhnfmhlknlaa",
-              "_blank",
-            );
-          }}
-        />
-      </div>
-    </div>
+    isVisible && (
+      <>
+        <div
+          className={`dark:bg-donutool-middle dark:text-donutool-bright absolute right-[1vw] bottom-[1vw] flex transform flex-col items-center justify-center gap-[1vw] rounded-[1vw] bg-gray-200 px-[4vw] py-[2vw] text-center text-[1vw] font-medium whitespace-pre-line text-neutral-600 shadow-lg transition-all duration-500 ease-in-out ${
+            isCollapsed
+              ? "translate-x-full opacity-0"
+              : "translate-x-0 opacity-100"
+          }`}
+        >
+          <button
+            onClick={handleClose}
+            className="dark:text-donutool-bright absolute top-[0.5vw] right-[0.5vw] flex h-[2vw] w-[2vw] cursor-pointer items-center justify-center rounded-full bg-gray-300 text-neutral-600 transition duration-300 hover:bg-gray-400"
+          >
+            <img src={CloseIcon} alt="close" className="h-[1vw] w-[1vw]" />
+          </button>
+
+          {t("textInModal")}
+          <ReusableButton
+            name={t("goToExtensionStore")}
+            onClick={() => {
+              window.open(
+                "https://chromewebstore.google.com/detail/donutool/heabnlejdfhggjiapbfmbhnfmhlknlaa",
+                "_blank",
+              );
+            }}
+          />
+        </div>
+        <div
+          className={`dark:bg-donutool-middle dark:text-donutool-bright absolute right-0 bottom-[5vw] flex h-[6vw] w-[2.5vw] transform cursor-pointer items-center justify-center rounded-l-[1vw] bg-gray-200 text-neutral-600 shadow-lg transition-all duration-500 ease-in-out ${
+            isCollapsed
+              ? "translate-x-0 opacity-100"
+              : "translate-x-full opacity-0"
+          }`}
+          onClick={handleShow}
+        >
+          <img src={ArrowLeftIcon} alt="show" className="h-[1.5vw] w-[1.5vw]" />
+        </div>
+      </>
+    )
   );
 }
